fd1f8e87363bf4e4b05699383ef60e5fad234309
Shukaria10 <ktasmiah10@gmail.com>
2020-07-17 02:43:33 +0600

    assignment complete

--- assignment.js
+++ assignment.js
@@ -1,47 +1,47 @@
-function feetToMile(feet)                                               //declaration of function
+function feetToMile(feet)                                               
 {
-    var mile = feet/5280;                                    //Equation of feet to mile convertion
+    var mile = feet/5280;                                    
     return mile;         
 
 }
-var output1 = feetToMile(5280);                                         //function call & output
-console.log(output1);                                                         //print the output
+var output1 = feetToMile(5280);                                  
+console.log(output1);                                                         
 
 
-function woodCalculator(chair,table,bed)                               //declaration of function
+function woodCalculator(chair,table,bed)                          
 {
-    var total = (chair*1) +(table*3) + (bed*5);            //Equation of total cubic feet of wood need
+    var total = (chair*1) +(table*3) + (bed*5);            
     return total;
 }
 
-var output2 = woodCalculator(2,2,2);                        //function call,parameter passing and output
-console.log(output2);                                                          //print the output
+var output2 = woodCalculator(2,2,2);                        
+console.log(output2);                                                         
 
 
-function brickCalculator(floor)                                         //declaration of function
+function brickCalculator(floor)                                         
 {  
-    if(floor<=10)                                                // if condition for 1-10th floor
+    if(floor<=10)                                                
     {
         totalBrick =15*1000;
     }
-    else if(floor>10 && floor<=20)                           // else-if condition for 11-20th floor
+    else if(floor>10 && floor<=20)                           
     {
         totalBrick = 12*1000;
     }
-    else                                                    //else condition for above 20th floor
+    else                                                    
     {                              
         totalBrick = 10*1000;
     }
     return totalBrick;
 }
-var output3 = brickCalculator(45);                           //output of total feet of brick need
+var output3 = brickCalculator(45);                       
 console.log(output3)
 
 
-function tinyFriend(name)                                            //declaration of function
+function tinyFriend(name)                                        
 {
  
- var min =name[0];                                             //indexing the intial first value
+ var min =name[0];                                        
  for(var i=0;i<=name.length;i++)
  {  var element = name[i];
     if(element < min)
@@ -53,5 +53,5 @@ function tinyFriend(name)                                            //declarati
  return min;
 
 }
-var output4 = tinyFriend([23,22,45,211]);                                //passing value & output
+var output4 = tinyFriend([23,22,45,211]);                         
 console.log(output4);
